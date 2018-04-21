9be4d171f0d5928476e77d11075ef356b0abcb43
LAPTOP-RIC\rikuz <mazzocchiriccardo@gmail.com>
2018-04-21 17:17:52 +0200

    Grid Generator draft and TurnManager creation

---
+++ Assets/Scripts/GridGenerator.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridGenerator : MonoBehaviour {
+
+    public int height;
+    public int width;
+
+    public Transform gridVisual;
+
+	// Use this for initialization
+	void Start () {
+        CreateGrid();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void CreateGrid()
+    {
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                Transform clone;
+                clone = Instantiate(gridVisual, new Vector3(x, y, 0), Quaternion.identity) as Transform;
+                clone.name = "Grid (" + x.ToString() + ", " + y.ToString() + ")";
+                clone.transform.parent = transform;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/TurnManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnManager : MonoBehaviour {
+
+    public enum TurnState { PlayerTurn, OtherTurn };
+
+    private TurnState _currentTurnState;
+    public TurnState CurrentTurnState
+    {
+        get
+        {
+            return _currentTurnState;
+        }
+        set
+        {
+            _currentTurnState = value;
+            OnTurnStart(_currentTurnState);
+        }
+    }
+
+
+    void OnTurnStart(TurnState newTurn)
+    {
+        switch (switch_on)
+        {
+            default:
+        }
+    }
+}

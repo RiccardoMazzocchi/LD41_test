7d275a9656466bf4acdf6cb61292b72b4d403b32
Riccardo Mazzocchi <rikuz91@hotmail.com>
2018-04-23 01:48:59 +0200

    no message

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -7,6 +7,7 @@ public class PlayerScript : MonoBehaviour {
     public enum Direction { Left, Right }
     Direction currentDirection;
 
+    public GameObject leftSlot, rightSlot, actionSlot; 
 
 	// Use this for initialization
 	void Start () {
@@ -31,7 +32,133 @@ public class PlayerScript : MonoBehaviour {
 
     public void Movement()
     {
+        //if run, vector3 ((-)1,0,0)
+        if (rightSlot.transform.childCount > 0)
+        {
+            if (rightSlot.transform.GetChild(0).GetComponent<Card>().cardData.name == "RunCard")
+            {
+                transform.position += new Vector3(1, 0, 0);
+                GameManager.Instance.tm.ChangeTurn();
+            }
 
+            else if (rightSlot.transform.GetChild(0).GetComponent<Card>().cardData.name == "JumpCard")
+            {
+                transform.position += new Vector3(1, 1, 0);
+
+                if (actionSlot.transform.childCount > 0)
+                {
+                    if (actionSlot.transform.GetChild(0).GetComponent<Card>().cardData.name == "DoubleJumpCard")
+                    {
+                        transform.position += new Vector3(1, 1, 0);
+                        GameManager.Instance.tm.ChangeTurn();
+                    }
+                }
+                else
+                {
+                    GameManager.Instance.tm.ChangeTurn();
+                }
+
+            }
+            else if (rightSlot.transform.GetChild(0).GetComponent<Card>().cardData.name == "HighJumpCard")
+            {
+                transform.position += new Vector3(1, 1, 0);
+
+                if (actionSlot.transform.childCount > 0)
+                {
+                    if (actionSlot.transform.GetChild(0).GetComponent<Card>().cardData.name == "DoubleJumpCard")
+                    {
+                        transform.position += new Vector3(1, 1, 0);
+                        GameManager.Instance.tm.ChangeTurn();
+                    }
+                }
+                else
+                {
+                    GameManager.Instance.tm.ChangeTurn();
+                }
+            }
+            else if (rightSlot.transform.GetChild(0).GetComponent<Card>().cardData.name == "JumpUpCard")
+            {
+                transform.position += new Vector3(0, 1, 0);
+                GameManager.Instance.tm.ChangeTurn();
+            }
+            else if (rightSlot.transform.GetChild(0).GetComponent<Card>().cardData.name == "StopCard")
+            {
+                transform.position += new Vector3(0, 0, 0);
+                GameManager.Instance.tm.ChangeTurn();
+            }
+        }
+
+
+        if (leftSlot.transform.childCount > 0)
+        {
+            if (leftSlot.transform.GetChild(0).GetComponent<Card>().cardData.name == "RunCard")
+            {
+                transform.position -= new Vector3(1, 0, 0);
+                GameManager.Instance.tm.ChangeTurn();
+            }
+            else if (leftSlot.transform.GetChild(0).GetComponent<Card>().cardData.name == "JumpCard")
+            {
+                transform.position += new Vector3(-1, 1, 0);
+                if (actionSlot.transform.childCount > 0)
+                {
+                    if (actionSlot.transform.GetChild(0).GetComponent<Card>().cardData.name == "DoubleJumpCard")
+                    {
+                        transform.position += new Vector3(-1, 1, 0);
+                        GameManager.Instance.tm.ChangeTurn();
+                    }
+                }
+                else
+                {
+                    GameManager.Instance.tm.ChangeTurn();
+                }
+            }
+            else if (leftSlot.transform.GetChild(0).GetComponent<Card>().cardData.name == "HighJumpCard")
+            {
+                transform.position += new Vector3(-1, 1, 0);
+                if (actionSlot.transform.childCount > 0)
+                {
+                    if (actionSlot.transform.GetChild(0).GetComponent<Card>().cardData.name == "DoubleJumpCard")
+                    {
+                        transform.position += new Vector3(-1, 1, 0);
+                        GameManager.Instance.tm.ChangeTurn();
+                    }
+                }
+                else
+                {
+                    GameManager.Instance.tm.ChangeTurn();
+                }
+            }
+            else if (leftSlot.transform.GetChild(0).GetComponent<Card>().cardData.name == "JumpUpCard")
+            {
+                transform.position += new Vector3(0, 1, 0);
+                GameManager.Instance.tm.ChangeTurn();
+            }
+            else if (leftSlot.transform.GetChild(0).GetComponent<Card>().cardData.name == "StopCard")
+            {
+                transform.position += new Vector3(0, 0, 0);
+                GameManager.Instance.tm.ChangeTurn();
+            }
+        }
+
+
+
+        //if jump, vector3 (0,1,0) + vector3 ((-)1,0,0) and drop
+        //if high jump, vector3 (0,1,0) + vector3 ((-)1,0,0) and then again vector3 (0,1,0) + vector3 ((-)1,0,0) and drop
+        //if jump up, vector3(0,1,0) and drop
+        //if stop, do nothing
+
+        //if double jump && jump OR high jump active, add (0,1,0)+((-)1,0,0)
+        //if direction swap, change card slot right>left or viceversa
+        //if punch, check if enemy is in adjacent tile & kill it
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy" || collision.gameObject.tag == "EnvironmentalDanger")
+        {
+            Destroy(gameObject);
+        }
     }
 
 }
